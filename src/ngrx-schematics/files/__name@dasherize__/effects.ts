fc5a96feb83fbb6aeb624427b5cbe226f8c0f205
first <firstnaruto@msn.com>
2018-07-27 20:33:26 +0700

    [-] Remove service comment

--- src/ngrx-schematics/files/__name@dasherize__/effects.ts
+++ src/ngrx-schematics/files/__name@dasherize__/effects.ts
@@ -3,7 +3,6 @@ import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Action } from '@ngrx/store';
 import { Observable, of as observableOf } from 'rxjs';
 import { catchError, map, startWith, switchMap } from 'rxjs/operators';
-//import { DataService } from '../../services/data.service';
 import * as featureActions from './actions';
 
 @Injectable()
